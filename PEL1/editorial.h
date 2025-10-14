63e9a9804736daadacdcba159b2216a66891f22f
MarioCastanoAlvarez <mario.castano.alvarez@gmail.com>
2025-10-14 18:17:44 +0200

    Add files via upload

---
+++ PEL1/editorial.h
@@ -0,0 +1,8 @@
+struct pedido{
+    int id_editorial;
+    string  id_pedido;
+    string cod_libro;
+    string materia;
+    int unidades;
+    string estado;
+};
---
+++ PEL1/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+//NOTAS: El programa no esta protegido contra la introducci�n de car�cteres no v�lidos para ser registrados como integers (ej: "a").
+
+using namespace std;
+bool esEntero(string entrada){
+    if(entrada.empty())return false;
+    for (char c : entrada){
+        if(!isdigit(c))return false;
+    } return true;
+};
+int main()
+{
+    string entrada;
+    int i;
+    do{
+    cout<<"=== MENU ==="<<endl
+    <<"1) Generar N pedidos"<<endl
+    <<"2) Paso (una FASE)"<<endl
+    <<"3) Mostrar estado"<<endl
+    <<"4) Paso COMPLETO (4 fases)"<<endl
+    <<"5) Ver caja de una libreria"<<endl
+    <<"0) Salir"<<endl<<endl
+    <<"Opcion: ";
+
+
+    getline(cin, entrada); cout<<endl;
+    if (esEntero(entrada)){
+        i = stoi(entrada);
+        switch(i){
+        case 0:
+            cout<<"Se ha elegido: 0) Salir"<<endl<<endl;
+            break;
+        case 1:
+            cout<<"Se ha elegido: 1) Generar N pedidos"<<endl<<endl;
+            break;
+        case 2:
+            cout<<"Se ha elegido: 2) Paso (una FASE)"<<endl<<endl;
+            break;
+        case 3:
+            cout<<"Se ha elegido: 3) Mostrar estado "<<endl<<endl;
+            break;
+        case 4:
+            cout<<"Se ha elegido: 4) Paso COMPLETO (4 fases)"<<endl<<endl;
+            break;
+        case 5:
+            cout<<"Se ha elegido: 5) Ver caja de una libreria"<<endl<<endl;
+            break;
+        default:
+            cout<<"La opcion seleccionada no esta contemplada, pruebe de nuevo."<<endl<<endl;
+    }cout<<"======================================================================="<<endl<<endl;
+    }else {cout<<"Se ha introducido un valor no valido. Porfavor, introduzca un valor entero."<<endl<<endl;
+    cout<<"======================================================================="<<endl<<endl;};
+    }while (i!=0);
+    cout<<"Fin del programa."<<endl<<endl;
+    return 0;
+}
