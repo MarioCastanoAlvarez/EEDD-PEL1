88d5d1295dcfa39ab66d1ce69de91008aa3464db
Lay3rr <coolcabit.gag@gmail.com>
2025-10-18 21:15:35 +0200

    Cambio en la cabecera (+Pila, +Cola)

--- PEL1/editorial.h
+++ PEL1/editorial.h
@@ -1,8 +1,119 @@
-struct pedido{
+#ifndef CABECERA_H_INCLUDED
+#define CABECERA_H_INCLUDED
+
+//Creación de estructura Pedido
+struct Pedido{
     int id_editorial;
-    string  id_pedido;
+    int id_pedido;
     string cod_libro;
     string materia;
     int unidades;
     string estado;
 };
+
+//Creación de clase Nodo
+class Nodo
+{
+private:
+
+    Pedido pedido;
+    Nodo *siguiente;
+    friend class Cola;
+    friend class Pila;
+
+    public:
+        Nodo(Pedido pd, Nodo *sig = NULL)
+        {
+            pedido = pd;
+            siguiente = sig;
+        }
+};
+typedef Nodo *pNodo;
+
+//Creación de clase Cola
+class Cola
+{
+    public:
+        Cola() : frente(NULL), final(NULL) {}
+        ~Cola();
+        void encolar(Pedido pd);
+        Pedido desencolar();
+
+    private:
+        pNodo frente, final;
+};
+
+Cola::~Cola()
+{
+    while(frente) desencolar();
+}
+
+void Cola::encolar(Pedido pd)
+{
+    pNodo nuevo;
+    nuevo = new Nodo(pd);
+
+    if(final) final->siguiente = nuevo;
+    final= nuevo;
+    if(!frente) frente = nuevo;
+}
+
+Pedido Cola::desencolar()
+{
+    pNodo nodo;
+    Pedido pd;
+    Pedido vacio = {0,0,"","",0,""};
+    nodo = frente;
+    frente = nodo->siguiente;
+    pd = nodo->pedido;
+
+    delete nodo;
+
+    if(!frente) final = NULL;
+    return pd;
+}
+
+//Creación de clase pila
+class Pila
+{
+    private:
+        pNodo cima;
+    public:
+        Pila() : cima(NULL) {}
+        ~Pila();
+        void apilar(Pedido pd);
+        Pedido desapilar();
+};
+
+Pila::~Pila()
+{
+    while(cima) desapilar();
+}
+
+void Pila::apilar(Pedido pd)
+{
+    pNodo nuevo;
+
+    nuevo = new Nodo(pd, cima);
+
+    cima = nuevo;
+}
+
+Pedido Pila::desapilar()
+{
+    pNodo nodo;
+    Pedido pd;
+    Pedido vacio = {0,0,"","",0,""};
+
+    if(!cima) return vacio;
+
+    nodo = cima;
+    cima = nodo->siguiente;
+    pd = nodo->pedido;
+
+    delete nodo;
+    return pd;
+}
+
+#endif // CABECERA_H_INCLUDED
+
