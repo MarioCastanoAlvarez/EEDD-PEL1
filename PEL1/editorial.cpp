d6ecfb5c2cf9c52e91a09135e258861803f81f90
MarioCastanoAlvarez <mario.castano.alvarez@gmail.com>
2025-11-01 18:16:18 +0100

    Correcion en editorial.cpp de la funcion cod_libroRandom()

--- PEL1/editorial.cpp
+++ PEL1/editorial.cpp
@@ -40,7 +40,8 @@ string cod_libroRandom(){
     string resultado="";
     for(int i = 0; i<3; ++i)
         resultado+=to_string(digitoRandom());
-    resultado+="K";
+    string alfabeto[] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    resultado+=alfabeto[rand()%26];
     for(int i = 0; i<2; ++i)
         resultado+=to_string(digitoRandom());
     return resultado;
